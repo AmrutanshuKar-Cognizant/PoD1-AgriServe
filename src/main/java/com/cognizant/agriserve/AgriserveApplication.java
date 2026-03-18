ccd924a2ac1a71d4177a0ad0582dde88d1530248
AmrutanshuKar-Cognizant <Amrutanshu.Kar@cognizant.com>
2026-03-18 09:10:46 +0530

    Revert "Karthik d"

--- src/main/java/com/cognizant/agriserve/AgriserveApplication.java
+++ src/main/java/com/cognizant/agriserve/AgriserveApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AgriserveApplication {
-
+	//hi this is karthik
 	public static void main(String[] args) {
 		SpringApplication.run(AgriserveApplication.class, args);
 	}
--- src/main/java/com/cognizant/agriserve/entity/AdvisoryContent.java
+++ src/main/java/com/cognizant/agriserve/entity/AdvisoryContent.java
@@ -4,7 +4,7 @@ import jakarta.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "advisoryContent")
+@Table(name = "AdvisoryContent")
 public class AdvisoryContent {
 
     @Id
