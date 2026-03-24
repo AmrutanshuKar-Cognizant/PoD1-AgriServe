d350c3eb9eec46d47e09d746d200e8b348f57725
Kar <2478084@cognizant.com>
2026-03-24 09:17:01 +0530

    Updated exception

--- src/main/java/com/cognizant/agriserve/exception/UnauthorizedActionException.java
+++ src/main/java/com/cognizant/agriserve/exception/UnauthorizedActionException.java
@@ -1,8 +1,12 @@
 package com.cognizant.agriserve.exception;
 
-public class UnauthorizedActionException extends RuntimeException {
-    public UnauthorizedActionException(String message) {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
+@ResponseStatus(value = HttpStatus.FORBIDDEN)
+public class UnauthorizedActionException extends RuntimeException {
+
+    public UnauthorizedActionException(String message) {
         super(message);
     }
 }
